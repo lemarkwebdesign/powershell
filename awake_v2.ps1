44192721b9cc2c66c5041b2308815fc6c16c4905
lemarkwebdesign <49920370+lemarkwebdesign@users.noreply.github.com>
2026-02-26 20:10:12 +0100

    Update awake_v2.ps1

--- awake_v2.ps1
+++ awake_v2.ps1
@@ -1,32 +1,7 @@
-# Definicja flag systemowych
-$KeepAliveCode = @"
-using System;
-using System.Runtime.InteropServices;
-
-public class User32 {
-    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    public static extern uint SetThreadExecutionState(uint esFlags);
-
-    public const uint ES_CONTINUOUS = 0x80000000;
-    public const uint ES_DISPLAY_REQUIRED = 0x00000002;
-    public const uint ES_SYSTEM_REQUIRED = 0x00000001;
-}
-"@
-
-Add-Type -TypeDefinition $KeepAliveCode
-
-Write-Host "Blokada wygaszacza aktywna. Ekran NIE zgaśnie." -ForegroundColor Cyan
-Write-Host "Zamknij to okno, aby przywrócić normalne ustawienia."
-
-# Pętla podtrzymująca stan aktywności co 30 sekund
-try {
-    while($true) {
-        # Informujemy system, że ekran i system są wymagane
-        [User32]::SetThreadExecutionState([User32]::ES_CONTINUOUS -bor [User32]::ES_DISPLAY_REQUIRED -bor [User32]::ES_SYSTEM_REQUIRED)
-        Start-Sleep -Seconds 30
-    }
-}
-finally {
-    # Przywrócenie domyślnego stanu po zamknięciu (opcjonalne, system sam to wyczyści po zamknięciu procesu)
-    [User32]::SetThreadExecutionState([User32]::ES_CONTINUOUS)
+$wsh = New-Object -ComObject WScript.Shell
+while($true) {
+  $wsh.SendKeys('{SCROLLLOCK}')
+  Start-Sleep -Milliseconds 100
+  $wsh.SendKeys('{SCROLLLOCK}')
+  Start-Sleep -Seconds 60
 }
